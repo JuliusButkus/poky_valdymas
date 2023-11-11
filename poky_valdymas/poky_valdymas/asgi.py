fdcbfeb3ad3799a1caa829e338191235f38aec5a
Julius Butkus <j.butkus.skouds@gmail.com>
2023-11-11 12:17:37 +0200

    veikia

---
+++ poky_valdymas/poky_valdymas/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for poky_valdymas project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poky_valdymas.settings')
+
+application = get_asgi_application()
